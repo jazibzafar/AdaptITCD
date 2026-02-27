b6b158f3891c31d0d5449baf542c4b5eece3b7d7
jazib <m.jazibzafar@stud.uni-goettingen.de>
2026-02-27 13:41:54 +0100

    even more detections per image

--- LitMaskRCNN.py
+++ LitMaskRCNN.py
@@ -183,7 +183,7 @@ class LitMaskRCNN(L.LightningModule):
                 rpn_anchor_generator=anchor_generator,
                 min_size=img_size,
                 max_size=img_size,
-                box_detections_per_img=150
+                box_detections_per_img=256
             )
         else:
             model = MaskRCNN(
