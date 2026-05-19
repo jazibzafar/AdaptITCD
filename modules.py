7f595e94c49b213b6a7ec6eaa8aabd115dab7935
jazib <m.jazibzafar@stud.uni-goettingen.de>
2026-05-19 18:04:33 +0200

    rollling back gradient checkpointing

--- modules.py
+++ modules.py
@@ -241,7 +241,6 @@ class ResNet50Backbone(nn.Module):
                r=lora_rank, lora_alpha=32, target_modules=["conv1", "conv2", "conv3"], lora_dropout=0.05, bias="none",
             )
             self.body = get_peft_model(self.body, lora_config)
-            self.body.gradient_checkpointing_enable()
             print("LoRA applied successfully")
             self.body.print_trainable_parameters()
 
@@ -271,7 +270,6 @@ class TorchvisionSwinV2Backbone(nn.Module):
                r=lora_rank, lora_alpha=32, target_modules=["qkv", "proj"], lora_dropout=0.05, bias="none",
             )
             base_model = get_peft_model(base_model, lora_config)
-            base_model.gradient_checkpointing_enable()
             print("LoRA applied successfully")
             base_model.print_trainable_parameters()
 
@@ -326,7 +324,6 @@ class ConvNeXtBackbone(nn.Module):
                r=lora_rank, lora_alpha=32, target_modules=["pwconv1", "pwconv2"], lora_dropout=0.05, bias="none",
             )
             base_model = get_peft_model(base_model, lora_config)
-            base_model.gradient_checkpointing_enable()
             print("LoRA applied successfully")
             base_model.print_trainable_parameters()
         self.body = base_model
